60ae23fbae94c2e070005f0b279700d32df8b0ec
davidboll <david.boll@me.com>
2024-04-09 15:01:28 +0200

    Adds welcome-image

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import styles from "./HomePage.module.css";
+import popcornImage from "../../assets/images/popcorn.png";
 
 const HomePage = () => (
   <div>
     <p className={styles.description}>
       Welcome to Popcorn Time! Take a look at our movies and shows.
     </p>
+    <img
+      className={styles.welcomeImage}
+      src={popcornImage}
+      alt="Image of popcorn"
+    />
   </div>
 );
 
