8b2f65cf86dab857d7f9ea4de5017eeab35dbd12
davidboll <david.boll@me.com>
2024-04-17 10:41:47 +0200

    Removes image from page

--- src/components/Page/index.jsx
+++ src/components/Page/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./Page.module.css";
 
-const Page = ({ title, content, image }) => (
+const Page = ({ title, content }) => (
   <div className={styles.container}>
     <h1 className={styles.title}>{title}</h1>
     <p className={styles.content}>{content}</p>
